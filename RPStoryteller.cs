8c6e5ff6dacb468f7de57f860bee8e5869fee83f
Christian Blouin <bongotastic@gmail.com>
2021-06-15 17:08:00 -0300

    Add project files.

---
+++ RPStoryteller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPStoryteller
+{
+    [KSPScenario(ScenarioCreationOptions.AddToNewCareerGames | ScenarioCreationOptions.AddToExistingCareerGames, GameScenes.SPACECENTER)]
+    public class RPStoryteller : ScenarioModule
+    {
+        public void Start()
+        {
+            KSPLog.print("[RPStoryteller] Hello World!");
+        }
+    }
+}
